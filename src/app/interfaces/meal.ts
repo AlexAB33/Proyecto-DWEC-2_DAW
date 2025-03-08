780bbb15e9e3bca46d8893d4172815ccc271d12e
AlexAB33 <alejandro.arcos-bellido@iesruizgijon.com>
2025-03-09 00:41:37 +0100

    Creadas interfaz y servicio de la API

---
+++ src/app/interfaces/meal.ts
@@ -0,0 +1,17 @@
+export interface IMeal {
+  idMeal: string;
+  strMeal: string;
+  strMealAlternate: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strTags: string[];
+  strYoutube: string;
+  strIngredients: string[];
+  strMeasures: string[];
+  strSource: string;
+  strImageSource: string;
+  strCreativeCommonsConfirmed: string;
+  dateModified: string;
+}
